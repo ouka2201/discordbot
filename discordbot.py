134147f6468b0c660fea5651f962f3f0e1d75b64
ouka2201 <61953493+ouka2201@users.noreply.github.com>
2020-03-09 10:51:35 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import os
 import traceback
+from grouping import MakeTeam
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -16,6 +17,20 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    
+@bot.command()
+async def team(ctx, specified_num=2):
+    make_team = MakeTeam()
+    remainder_flag = 'true'
+    msg = make_team.make_party_num(ctx,specified_num,remainder_flag)
+    await ctx.channel.send(msg)
+# メンバー数が均等にはならないチーム分け
+@bot.command()
+async def team_norem(ctx, specified_num=2):
+    make_team = MakeTeam()
+    msg = make_team.make_party_num(ctx,specified_num)
+    await ctx.channel.send(msg)
+
 
 
 bot.run(token)
