90942f0b7e60de89f349bd7b5b10dd8fe432b56e
ouka2201 <61953493+ouka2201@users.noreply.github.com>
2020-04-20 13:37:00 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -53,7 +53,7 @@ async def p (ctx,*args):
 	pop.add_field(name="一匹め",value=name1,inline=False)
 	pop.add_field(name="二匹め",value=name2,inline=False)
 	
-	await bot.send_message(CHANNEL, embed=pop)
+	await CLIENT.send_message(CHANNEL, embed=pop)
 	
 @bot.event
 async def regular_processing():
