7108d464166b09e9fc64b33644e92cba16162db5
ouka2201 <61953493+ouka2201@users.noreply.github.com>
2020-04-20 13:28:45 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,8 +9,7 @@ import csv
 import pandas as pd
 import discord
 
-
-
+CHANNEL = discord.Object(id=618007010071543809)
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 player_list = []
@@ -53,7 +52,7 @@ async def p (ctx,*args):
 	pop.add_field(name="一匹め",value=name1,inline=False)
 	pop.add_field(name="二匹め",value=name2,inline=False)
 	
-	await channel.send(embed=pop)
+	await CHANNEL.send(embed=pop)
 	
 @bot.event
 async def regular_processing():
