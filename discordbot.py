c094ddc8684fa1ec0628053ea9a9dfc95dbafa4d
ouka2201 <61953493+ouka2201@users.noreply.github.com>
2020-04-20 10:37:20 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -19,6 +19,7 @@ async def on_command_error(ctx, error):
 	orig_error = getattr(error, "original", error)
 	error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
 	await ctx.send(error_msg)
+				 
 								 
 @bot.command()
 async def ping(ctx):
