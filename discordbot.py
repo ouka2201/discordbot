22a620eb07720c2567e070943498e69063a13889
ouka2201 <61953493+ouka2201@users.noreply.github.com>
2020-04-20 10:46:24 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,7 +9,8 @@ import csv
 import pandas as pd
 import discord
 
-df = pd.read_csv(pop.csv", index_col=0)
+
+
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 player_list = []
@@ -19,7 +20,6 @@ async def on_command_error(ctx, error):
 	orig_error = getattr(error, "original", error)
 	error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
 	await ctx.send(error_msg)
-				 
 								 
 @bot.command()
 async def ping(ctx):
@@ -45,12 +45,23 @@ async def s (ctx,*args):
 
 	await ctx.send(embed=keka)
     
+@bot.command()
+async def p (ctx,*args):
+	name1,name2,time = nextpop(0,10,40)
+	pop = discord.Embed(title="pop")
+	keka.add_field(name="時間",value=time,inline=False)
+	keka.add_field(name="一匹め",value=name1,inline=False)
+	keka.add_field(name="二匹め",value=name2,inline=False)
+	
+	await ctx.send(embed=keka)
+	
 def nextpop(wday,hour,min):
- 	df = pd.read_csv(pop.csv", index_col=0)
+ 	df = pd.read_csv("pop.csv", index_col=0)
 	df.query('wday == @wday & hour == @hour % min == @min', inplace=True)
 	name1 = df['name1'].values[0]
 	name2 = df['name12'].values[0]
 	time = df['time'].values[0]
 
 	return name1,name2,time
+					 
 bot.run(token)
