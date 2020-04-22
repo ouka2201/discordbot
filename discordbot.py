42c460d622f71d949d76b30cc2dbe856d54daac5
ouka2201 <61953493+ouka2201@users.noreply.github.com>
2020-04-22 14:08:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,21 +17,20 @@ prefix = '-'
 
 class JapaneseHelpCommand(commands.DefaultHelpCommand):
 	def __init__(self):
-	super().__init__()
-	self.commands_heading = "コマンド:"
-	self.no_category = "その他"
-	self.command_attrs["help"] = "コマンド一覧と簡単な説明を表示"
-
+		super().__init__()
+		self.commands_heading = "コマンド:"
+		self.no_category = "その他"
+		self.command_attrs["help"] = "コマンド一覧と簡単な説明を表示"
 	def get_ending_note(self):
-	return (f"各コマンドの説明: {prefix}help <コマンド名>\n"f"各カテゴリの説明: {prefix}help <カテゴリ名>\n")
+		return (f"各コマンドの説明: {prefix}help <コマンド名>\n"f"各カテゴリの説明: {prefix}help <カテゴリ名>\n")
 
 bot = commands.Bot(command_prefix=prefix, help_command=JapaneseHelpCommand())
 
 @bot.event
 async def on_ready():
-    print("on_ready")
-    print(discord.__version__)
-    mashas.setup(bot)
+	print("on_ready")
+	print(discord.__version__)
+	mashas.setup(bot)
 
 @bot.event
 async def regular_processing():
