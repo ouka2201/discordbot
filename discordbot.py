932232504287cd1fcf90e4714b97304b2c7ae2ff
ouka2201 <61953493+ouka2201@users.noreply.github.com>
2020-04-22 15:00:00 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -23,7 +23,7 @@ class JapaneseHelpCommand(commands.DefaultHelpCommand):
 		self.no_category = "その他"
 		self.command_attrs["help"] = "コマンド一覧と簡単な説明を表示"
 	def get_ending_note(self):
-		return (f"各コマンドの説明: {prefix}help <コマンド名>\n"f"各カテゴリの説明: {prefix}help <カテゴリ名>\n")
+		return (f"各コマンドの説明: {prefix}help <コマンド名>\n"f"各カテゴリの説明: {prefix}help <カテゴリ名>\n"f"会話用で話すと何かしら返してくれるよ！\n")
 
 bot = commands.Bot(command_prefix=prefix, help_command=JapaneseHelpCommand())
 
