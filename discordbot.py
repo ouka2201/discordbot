a34cc933c727486d2da958eea0aef39932323b24
ouka2201 <61953493+ouka2201@users.noreply.github.com>
2020-04-22 12:25:24 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -46,16 +46,10 @@ async def regular_processing():
 				pass
 			
 		await sleep(60)
-
-		
-@bot.event
-async def on_command_error(ctx, error):
-	orig_error = getattr(error, "original", error)
-	error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-	await ctx.send(error_msg)
 								 
 @bot.command()
 async def ping(ctx):
+	"""まこんが挨拶するだけ"""
 	embed = discord.Embed(description="おはようございますぅ")
 	embed.set_author(name="雅/Mashas.",icon_url="https://cdn.discordapp.com/attachments/562098530366390275/701668974114504745/442d2198c53f8e1d.png")
 	
@@ -63,6 +57,7 @@ async def ping(ctx):
     
 @bot.command()
 async def s (ctx,*args):
+	"""空白入れながら名前書いてくと分けてくれる"""
 	player_list = list(args)
 	random.shuffle(player_list)  
 	n = math.ceil(len(player_list) / 2)
@@ -81,6 +76,7 @@ async def s (ctx,*args):
 
 @bot.command(name="占い")
 async def uranai(ctx):
+	"""占うやつ"""
 	lucks = ["大吉", "中吉", "小吉", "吉", "凶", "大凶"]
 	luck = random.choice(lucks)
 
@@ -99,18 +95,9 @@ async def uranai(ctx):
 
 	await ctx.send(embed=keka)
 
-@bot.command()
-async def p (ctx,*args):
-	now = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9)))
-	name1, name2, time = nextpop(now.weekday(), now.hour, now.minute)
-	pop = discord.Embed(title="pop")
-	pop.add_field(name="時間",value=time,inline=False)
-	pop.add_field(name="一匹め",value=name1,inline=False)
-	pop.add_field(name="二匹め",value=name2,inline=False)
-	
-	await ctx.send(embed=pop)
 @bot.command(name="t")
 async def trans(ctx, *, arg):
+	"""まこんが翻訳してくれる"""
 	str = arg
 	detect = translator.detect(str)
 	befor_lang = detect.lang
@@ -135,6 +122,7 @@ async def trans(ctx, *, arg):
 
 @bot.command(name="d")
 async def detectbot(ctx, *, arg):
+	"""まこんが何語か解説してくれる"""
 	detect = translator.detect(arg)
 	m = detect.lang + ' ですぅ'
 	embed = discord.Embed(title="「言語解析結果ですぅ」")
