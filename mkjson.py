3afbec833d039bdd880348d9b448f332b84ac4ab
ouka2201 <61953493+ouka2201@users.noreply.github.com>
2020-04-23 09:50:09 +0900

    Create mkjson.py

---
+++ mkjson.py
@@ -0,0 +1,31 @@
+import json
+
+def mkcplus(self, content, name):
+	payload = {
+            "utterance": content,
+            "username": name,
+            "agentState": {
+                "agentName": "色々できるBOT",
+                "tone": "normal",
+                "age": "22歳"},
+            "addition": {
+                "unknownResponses": [
+                    "どゆこと？"
+                ],
+                "ngwords": [
+                    "肩凝った"
+                ],
+                "utterancePairs": [
+                    {
+                    "utterance": "肩凝った",
+                    "response": "適度に運動しないとね"
+                    }, {
+                    "utterance": "シージ",
+                    "response": "消しちゃえ,まだそのクソゲーやってるの？,やめちゃえ"
+                    }
+                ]
+            }
+        }
+
+    enc = json.dumps(payload)
+    return enc
