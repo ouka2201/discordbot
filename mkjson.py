024f98826aa8755dd139f6afb14aa5e9333af934
ouka2201 <61953493+ouka2201@users.noreply.github.com>
2020-04-23 10:08:55 +0900

    Update mkjson.py

--- mkjson.py
+++ mkjson.py
@@ -1,31 +1,30 @@
 import json
 
-def mkcplus(self, content, name):
+def mkcplus(content, name):
 	payload = {
-            "utterance": content,
-            "username": name,
-            "agentState": {
-                "agentName": "色々できるBOT",
-                "tone": "normal",
-                "age": "22歳"},
-            "addition": {
-                "unknownResponses": [
-                    "どゆこと？"
-                ],
-                "ngwords": [
-                    "肩凝った"
-                ],
-                "utterancePairs": [
-                    {
-                    "utterance": "肩凝った",
-                    "response": "適度に運動しないとね"
-                    }, {
-                    "utterance": "シージ",
-                    "response": "消しちゃえ,まだそのクソゲーやってるの？,やめちゃえ"
-                    }
-                ]
-            }
-        }
-
-    enc = json.dumps(payload)
-    return enc
+		"utterance": content,
+		"username": name,
+		"agentState": {
+		"agentName": "色々できるBOT",
+		"tone": "normal",
+		"age": "22歳"},
+	"addition": {
+		"unknownResponses": [
+			"どゆこと？"
+		],
+		"ngwords": [
+			"肩凝った"
+		],
+		"utterancePairs": [
+			{
+			"utterance": "肩凝った",
+			"response": "適度に運動しないとね"
+			}, {
+			"utterance": "シージ",
+			"response": "消しちゃえ,まだそのクソゲーやってるの？,やめちゃえ"
+			}
+		]
+		}
+	}
+	enc = json.dumps(payload)
+	return enc
