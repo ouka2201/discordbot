3b213f25e9ba62fadfb651af6810830f88f05c05
ouka2201 <61953493+ouka2201@users.noreply.github.com>
2020-05-25 14:52:01 +0900

    Update audio.py

--- cogs/audio.py
+++ cogs/audio.py
@@ -6,19 +6,19 @@ class audio(commands.Cog, name='読み上げ'):
     super().__init__()
     self.bot = bot
         
-@commands.command(name="join")
-async def join(self, ctx):
-  """botがチャンネルに接続する"""
-  # voicechannelを取得
-  vc = ctx.author.voice.channel
-  # voicechannelに接続
-  await vc.connect()
+  @commands.command(name="join")
+  async def join(self, ctx):
+    """botがチャンネルに接続する"""
+    # voicechannelを取得
+    vc = ctx.author.voice.channel
+    # voicechannelに接続
+    await vc.connect()
 
-@commands.command(name="bye")
-async def bye(self, ctx):
-  """botが切断"""
-  # 切断
-  await ctx.voice_client.disconnect()
+  @commands.command(name="bye")
+  async def bye(self, ctx):
+    """botが切断"""
+    # 切断
+    await ctx.voice_client.disconnect()
 
 def setup(bot):
   print("起動")
