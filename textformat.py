4da9bd7752861bf260fd6ff54bb87ee8e4d43ddc
ouka2201 <61953493+ouka2201@users.noreply.github.com>
2020-05-25 16:25:23 +0900

    Update textformat.py

--- textformat.py
+++ textformat.py
@@ -4,6 +4,7 @@ import emoji
 def format_text(text):
     text = re.sub(r'https?://[\w/:%#\$&\?\(\)~\.=\+\-…]+', "", text)
     text = re.sub(r'<:[a-zA-Z0-9_]+:[0-9]+>', "", text)
+    text = re.sub(r'<@![0-9]+>', "", text)
     text = re.sub('RT', "", text)
     text = re.sub('お気に入り', "", text)
     text = re.sub('まとめ', "", text)
