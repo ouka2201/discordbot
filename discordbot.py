f697dc9198301717e4f7c0460e2be007935a6e07
ouka2201 <61953493+ouka2201@users.noreply.github.com>
2020-05-26 10:44:36 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -90,15 +90,19 @@ async def on_message(message):
 					if text == "":
 						print(text)
 					else:
-						txt = message.author.name + "のメッセージです。" + text
-						makemp3(txt)
+						if message.content.startswith('-e'):
+							txt = selectName(message.author.id) + "のメッセージです。" + text
+							makemp3e(txt)
+						else:
+							txt = selectName(message.author.id) + "のメッセージです。" + text
+							makemp3(txt, selectVoice(message.author.id))
 						source = discord.FFmpegPCMAudio("output.mp3")
 						message.guild.voice_client.play(source)
 				else:
 					pass
 	await bot.process_commands(message)
 	
-def makemp3(str):
+def makemp3(str, name):
 	# クライアントの作成
 	client = texttospeech.TextToSpeechClient()
 
@@ -108,13 +112,13 @@ def makemp3(str):
 	# ボイスのリクエスト nameを書き換えれば他の音声に変更可能
 	voice = texttospeech.types.VoiceSelectionParams(
 		language_code='ja-JP',
-		name='ja-JP-Wavenet-A',
+		name=name,
 		ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
 
 	# オーディオコンフィグ
 	audio_config = texttospeech.types.AudioConfig(
 		audio_encoding=texttospeech.enums.AudioEncoding.MP3,
-		speaking_rate=1.8)
+		speaking_rate=1.5)
 
 	# 色々まとめる
 	response = client.synthesize_speech(synthesis_input, voice, audio_config)
@@ -123,5 +127,79 @@ def makemp3(str):
 	with open('output.mp3', 'wb') as out:
 		out.write(response.audio_content)
 		print('Audio content written to file "output.mp3"')
+		
+def makemp3e(str):
+	# クライアントの作成
+	client = texttospeech.TextToSpeechClient()
+
+	# 引数のテキストをセット
+	synthesis_input = texttospeech.types.SynthesisInput(text=str)
+
+	# ボイスのリクエスト nameを書き換えれば他の音声に変更可能
+	voice = texttospeech.types.VoiceSelectionParams(
+		language_code='en-US',
+		name='en-US-Wavenet-A',
+		ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
+
+	# オーディオコンフィグ
+	audio_config = texttospeech.types.AudioConfig(
+		audio_encoding=texttospeech.enums.AudioEncoding.MP3,
+		speaking_rate=1.3
+		)
+
+	# 色々まとめる
+	response = client.synthesize_speech(synthesis_input, voice, audio_config)
+
+	# バイナリ書き込み
+	with open('output.mp3', 'wb') as out:
+		out.write(response.audio_content)
+		print('Audio content written to file "output.mp3"')
+		
+		
+def selectName(id):
+	dect = {259309338126974976: "おうか",
+		197957553555111936: "はや",
+		416615433517400074: "たけだ",
+		345501651039289354: "けい",
+		400857984697368578: "こてぃ",
+		450957905273815040: "いっくん",
+		375876355885367297: "おむらいす",
+		459944123915567144: "りくどー",
+		447024643732144128: "ごっど",
+		379636611207659540: "ゆっけ",
+		270109037926940672: "てんどん",
+		538941724786032640: "まこん",
+		332668918261088266: "ちゃぼ",
+		526597888793378828: "りょう",
+		483302986164994075: "あこん",
+		460013158317162496: "オリヴィエ",
+		386547269568036866: "しりうす",
+		417712684172312584: "たか",
+		540101055673925642: "がすてぃ"
+		}
+	return dect[id]
+
+def selectVoice(id):
+	dect = {259309338126974976: "ja-JP-Standard-A",
+		197957553555111936: "ja-JP-Standard-B",
+		416615433517400074: "ja-JP-Standard-C",
+		345501651039289354: "ja-JP-Standard-D",
+		400857984697368578: "ja-JP-Wavenet-B",
+		450957905273815040: "ja-JP-Wavenet-C",
+		375876355885367297: "ja-JP-Wavenet-A",
+		459944123915567144: "ja-JP-Wavenet-D",
+		447024643732144128: "ja-JP-Wavenet-A",
+		379636611207659540: "ja-JP-Wavenet-A",
+		270109037926940672: "ja-JP-Wavenet-A",
+		538941724786032640: "ja-JP-Wavenet-A",
+		332668918261088266: "ja-JP-Wavenet-B",
+		526597888793378828: "ja-JP-Wavenet-B",
+		483302986164994075: "ja-JP-Wavenet-B",
+		460013158317162496: "ja-JP-Wavenet-B",
+		386547269568036866: "ja-JP-Wavenet-B",
+		417712684172312584: "ja-JP-Wavenet-B",
+		540101055673925642: "ja-JP-Wavenet-B"
+		}
+	return dect[id]
 	
 bot.run(token)
